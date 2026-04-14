9122abf71ee2c5c11f52a06fdf3d766e9b715b8b
cstuart756 <stujjc@yahoo.co.uk>
2026-04-14 18:49:50 +0100

    feat: add high-converting SaaS landing page

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+def landing(request):
+    return render(request, "landing.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('leads.urls')),
+    path('', landing, name='landing'),
+    path('leads/', include('leads.urls')),
     path('dashboard/', include('dashboard.urls')),
 ]
