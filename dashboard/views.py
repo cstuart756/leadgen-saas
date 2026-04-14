c47d5c4c0f73be8600c1ef249123cea880c0c4f5
cstuart756 <stujjc@yahoo.co.uk>
2026-04-14 18:33:07 +0100

    feat: integrate leads into dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from leads.models import Lead
 
-# Create your views here.
+@login_required
+def dashboard(request):
+	leads = Lead.objects.filter(user=request.user)
+	count = leads.count()
+	return render(request, 'dashboard/dashboard.html', {'leads_count': count})
