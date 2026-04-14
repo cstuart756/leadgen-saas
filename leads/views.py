926541e6b9b382b687728ad73859af5fc9e13808
cstuart756 <stujjc@yahoo.co.uk>
2026-04-14 18:32:33 +0100

    feat: add public lead capture page per user

--- leads/views.py
+++ leads/views.py
@@ -8,15 +8,14 @@ from django.contrib.auth.decorators import login_required
 def lead_capture(request, username):
 	user = get_object_or_404(User, username=username)
 	if request.method == 'POST':
-		form = LeadForm(request.POST)
-		if form.is_valid():
-			lead = form.save(commit=False)
-			lead.user = user
-			lead.save()
-			return render(request, 'leads/thank_you.html', {'user': user})
-	else:
-		form = LeadForm()
-	return render(request, 'leads/lead_form.html', {'form': form, 'user': user})
+		Lead.objects.create(
+			user=user,
+			name=request.POST.get('name'),
+			email=request.POST.get('email'),
+			message=request.POST.get('message')
+		)
+		return render(request, 'leads/thank_you.html', {'user': user})
+	return render(request, 'leads/public_form.html', {'user': user})
 
 @login_required
 def lead_list(request):
